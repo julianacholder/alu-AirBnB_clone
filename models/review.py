dbb50ec7fd8c04a0ccafb790e095704ee93d19a5
bniyomukiz <b.niyomukiz@alustudent.com>
2024-01-14 08:01:55 -0800

    done

--- models/review.py
+++ models/review.py
@@ -2,7 +2,6 @@
 """ Review module for the HBNB project """
 from models.base_model import BaseModel
 
-
 class Review(BaseModel):
     """ Review classto store review information """
     place_id = ""
